dbfb5185338b0159efb983070e0a1f92f4ee517b
Rocío Córdova <stefnova@la-iMac-de-Rocio.local>
2017-03-12 12:02:43 -0500

    Initial Commit

---
+++ Tarea 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea 4
+//
+//  Created by Rocío Córdova on 12/03/17.
+//  Copyright © 2017 Rocío Córdova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
